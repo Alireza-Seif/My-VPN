4ca83cb14d2646d2e8199edbd948abf0bd1df1b1
alirezaseif20 <alirezaariye@gmail.com>
2024-03-17 22:32:16 +0330

    feat:add VPN Status & VPN Configuration class Implementation

---
+++ lib/models/vpn_configuration.dart
@@ -0,0 +1,11 @@
+class VpnConfiguration {
+  final String userName, password, countryName, config;
+
+  VpnConfiguration({
+    required this.userName,
+    required this.password,
+    required this.countryName,
+    required this.config,
+  });
+
+}
