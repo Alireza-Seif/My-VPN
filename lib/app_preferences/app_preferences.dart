433d89f9799a1f686732fb98adf3a15cadc49cf1
alirezaseif20 <alirezaariye@gmail.com>
2024-03-17 21:13:07 +0330

    feat: add Save single selected VPN Server Detail

--- lib/app_preferences/app_preferences.dart
+++ lib/app_preferences/app_preferences.dart
@@ -1,24 +1,46 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_vpn/models/vpn_info.dart';
 
-class AppPreferences 
-{
+class AppPreferences {
   static late Box boxOfData;
 
   static Future<void> initHive() async {
-
     await Hive.initFlutter();
 
-    boxOfData = await Hive.openBox('data'); 
+    boxOfData = await Hive.openBox('data');
   }
 
   //saving user choice about theme selection
   static bool get isModeDark => boxOfData.get('isModeDark') ?? false;
-  static set isModeDark(bool value) => boxOfData.put('isModeDark', value); 
+  static set isModeDark(bool value) => boxOfData.put('isModeDark', value);
+
+  // for saving single selected vpn details
+  static VpnInfo get vpnInfoObj =>
+      VpnInfo.fromJson(jsonDecode(boxOfData.get('vpn') ?? '{}'));
+  static set vpnInfoObj(VpnInfo value) =>
+      boxOfData.put('vpn', jsonEncode(value));
+
+  // for saving all vpn servers details
+  static List<VpnInfo> get vpnList {
+    List<VpnInfo> tempVpnList = [];
+    final dataVpn = jsonDecode(boxOfData.get('vpnList') ?? '[]');
+
+    for (var data in dataVpn) {
+      tempVpnList.add(VpnInfo.fromJson(data));
+    }
+    return tempVpnList;
+  }
+
+  static set vpnList(List<VpnInfo> valueList) =>
+      boxOfData.put('vpnList', jsonEncode(valueList));
 }
 
-
-extension AppTheme on ThemeData{
-  Color get lightTextColor => AppPreferences.isModeDark ? Colors.white70 : Colors.black54;
-  Color get bottomNavigationColor => AppPreferences.isModeDark ? Colors.white12 : Colors.redAccent;
+extension AppTheme on ThemeData {
+  Color get lightTextColor =>
+      AppPreferences.isModeDark ? Colors.white70 : Colors.black54;
+  Color get bottomNavigationColor =>
+      AppPreferences.isModeDark ? Colors.white12 : Colors.redAccent;
 }
