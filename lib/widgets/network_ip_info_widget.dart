4cb1c27842770af73d17add6ae4142d0ec4e48bc
alirezaseif20 <alirezaariye@gmail.com>
2024-03-23 18:13:47 +0330

    feat: add Network IP Info Model Class

---
+++ lib/widgets/network_ip_info_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_vpn/main.dart';
+import 'package:my_vpn/models/network_ip_info.dart';
+
+class NetworkIPInfoWidget extends StatelessWidget {
+  const NetworkIPInfoWidget({super.key, required this.networkIPInfo});
+
+  final NetworkIPInfo networkIPInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    sizeScreen = MediaQuery.of(context).size;
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.symmetric(vertical: sizeScreen.height * .01),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        leading: Icon(
+          networkIPInfo.iconData.icon,
+          size: networkIPInfo.iconData.size ?? 28,
+        ),
+        title: Text(
+          networkIPInfo.titleText,
+        ),
+        subtitle: Text(networkIPInfo.subtitleText),
+      ),
+    );
+  }
+}
